1a6ff59f3c49c2fef6fad40af4973e661e8d67c2
alberto <alberto@localhost>
2013-08-01 00:38:04 +0100

    first end to end test!!

--- mod-api/src/main/java/com/navid/trafalgar/input/KeyboardCommandStateListener.java
+++ mod-api/src/main/java/com/navid/trafalgar/input/KeyboardCommandStateListener.java
@@ -6,6 +6,7 @@ package com.navid.trafalgar.input;
 
 import com.jme3.input.InputManager;
 import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.navid.trafalgar.manager.PrestartState;
 
@@ -16,7 +17,7 @@ import com.navid.trafalgar.manager.PrestartState;
 public class KeyboardCommandStateListener implements CommandStateListener, PrestartState {
 
     private InputManager inputManager;
-    private ActionListener listener;
+    private AnalogListener listener;
     private int keycode;
     private Command key;
 
@@ -37,21 +38,19 @@ public class KeyboardCommandStateListener implements CommandStateListener, Prest
 
     @Override
     public void onPrestart(float tpf) {
-        listener = new ActionListener() {
+        listener = new AnalogListener() {
             @Override
-            public void onAction(String string, boolean bln, float tpf) {
-                //if (bln) {
-                    key.execute(tpf);
-               // }
+            public void onAnalog(String string, float f, float f1) {
+                key.execute(f);
             }
         };
 
         inputManager.addListener(listener, key.toString());
         inputManager.addMapping(key.toString(), new KeyTrigger(keycode));
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return key.toString();
     }
 }
